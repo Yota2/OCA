379bae08cddb7a64e3cac8b0ff136a48918bd1b3
Yota <yoni.jmr@gmail.com>
2022-08-18 11:06:49 +0200

    Variables

---
+++ Variables.java
@@ -0,0 +1,16 @@
+public class Variables {
+    /*
+    Which of the following are valid Java identifiers?
+ A$B_---> possible to have underscores
+ _helloWorld
+ true    // is a reserved word but if he said True -- Capital “True” is not a reserved word so it will work.
+ java.lang // can not be used in the
+ Public
+ 1980_s//they can not start with numbers it can possible only starts with _$or letter not a number
+ 1_0_0_ --> also possible to finish with underscore but not with the dote
+     */
+    public static void main(String[] args) {
+        double er_1_1_1_ = 12.3;
+        double er_1_1_1.0 = 12.3;//error coz of the dot at the end and
+    }
+}
