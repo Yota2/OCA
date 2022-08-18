4eaa7d42d12305e5e477b3887ec5152baa500d3d
Yota <yoni.jmr@gmail.com>
2022-08-18 11:36:13 +0200

    redeclaring variable in loops

---
+++ Loops.java
@@ -0,0 +1,7 @@
+public class Loops {
+    int i = 0;
+        for(long y = 5, i = 4; x < 7 && y < 40; x++, y++) {
+        // DOES NOT COMPILE coz redeclaring a Variable in the Initialization Block is not allowed.
+        System.out.print(x + " ");
+    }
+}
